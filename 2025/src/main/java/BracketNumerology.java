72cb229766bf3fde7b7b1b82e52436f44c53c539
Cross, Winter Isaac <wi928rsnowflake@gmail.com>
2025-03-15 17:45:34 -0400

    Implemented full tournament simulation

--- 2025/src/main/java/BracketNumerology.java
+++ 2025/src/main/java/BracketNumerology.java
@@ -1,8 +1,11 @@
+import java.sql.Array;
 import java.util.HashMap;
 import java.util.Map;
 import com.opencsv.CSVReader;
 import java.io.*;
 import java.util.Random;
+import java.util.ArrayList;
+import java.util.List;
 
 public class BracketNumerology {
     Map<Character, Double> letterScores = new HashMap<>();
@@ -57,10 +60,75 @@ public class BracketNumerology {
 
 
     public void simulate_tournament(String teamCsvPath){
+        //load csv data into an object
+        try (FileReader fileReader = new FileReader(teamCsvPath)) {
+            //!!Should really be a list of one class which stores team name and round number
+            ArrayList<String> bracketTeams = new ArrayList<String>();
+            ArrayList<Integer> bracketRounds = new ArrayList<Integer>();
 
-    }
-    public void simulate_round(String teamCsvPath){
+            CSVReader csvReader = new CSVReader(fileReader);
+            //cycle through lines of csv
+            String[] nextLine = csvReader.readNext();//Skip the header
+            while ((nextLine = csvReader.readNext()) != null) {
+                bracketTeams.add(nextLine[0]);
+                bracketRounds.add(Integer.parseInt(nextLine[2]));
+            }
 
+            //Simulate rounds until the tournament ends
+            boolean keepgoing = true;
+            while (keepgoing){
+                System.out.println("Round start");
+
+                //find lowest round number
+                Integer roundNumber = null;
+                for (int i=0; i<bracketRounds.size(); i++){
+                    int round = bracketRounds.get(i);
+                    if (roundNumber==null || round<roundNumber) {roundNumber = round;}
+                }
+
+                //Generate the bracket of the next round by simulating matches
+                ArrayList<String> newBracketTeams = new ArrayList<String>();
+                ArrayList<Integer> newBracketRounds = new ArrayList<Integer>();
+
+                String savedTeam = null;
+                for (int i=0; i<bracketTeams.size(); i++){
+                    String team = bracketTeams.get(i);
+                    int round = bracketRounds.get(i);
+                    if (round != roundNumber){
+                        //Leave this entry unchanged
+                        newBracketTeams.add(team);
+                        newBracketRounds.add(round);
+                    }else{
+                        //simulate match with adjacent team
+                        if (savedTeam==null){
+                            //competitor not found yet. Save this team's data
+                            savedTeam = team;
+                        }else{
+                            //competitor already found. Simulate this match
+                            int winningTeamIndex = simulate_match(savedTeam, team);
+                            if (winningTeamIndex==0){
+                                newBracketTeams.add(savedTeam);
+                            }else{
+                                newBracketTeams.add(team);
+                            }
+                            newBracketRounds.add(roundNumber+1);
+                            //clear saved team data
+                            savedTeam = null;
+                        }
+                    }
+                }
+
+                //check if tournament is over
+                if (newBracketTeams.size()<=1) {
+                    keepgoing = false;
+                }else{
+                    bracketTeams = newBracketTeams;
+                    bracketRounds = newBracketRounds;
+                }
+            }//endWhile
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
     public int simulate_match(String team0, String team1){
         //Return 0 if team0 wins, 1 if team1 wins
@@ -92,7 +160,7 @@ public class BracketNumerology {
     public static void main(String[] args){
         BracketNumerology numerology = new BracketNumerology();
         numerology.generate_letter_scores("Teams.csv");
-//        numerology.simulate_tournament("Teams.csv");
+        numerology.simulate_tournament("Teams.csv");
 //        System.out.println(numerology.simulate_match("ae","aa"));
 //        System.out.print(numerology.get_name_normalized_score("aea"));
     }
